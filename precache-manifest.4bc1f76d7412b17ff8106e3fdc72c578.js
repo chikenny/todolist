c2bc0f0eda49a03f1b19a67a176c474eb768b9f9
chikenny <chikenny@gmail.com>
2020-09-13 12:03:18 -0400

    Updates

---
+++ precache-manifest.4bc1f76d7412b17ff8106e3fdc72c578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cee69e982084097c845787c42263351",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a506a7c14d1c36b155a6",
+    "url": "/todolist/static/css/main.851d412d.chunk.css"
+  },
+  {
+    "revision": "854dd03ebe71d7ace674",
+    "url": "/todolist/static/js/2.e594a27f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.e594a27f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a506a7c14d1c36b155a6",
+    "url": "/todolist/static/js/main.ce60e207.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
